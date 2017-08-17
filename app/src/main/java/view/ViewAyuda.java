e65102d8212cc9d418fe61f37857bfc352d97dd1
FLegaza <flegazab@gmail.com>
2017-08-17 21:38:33 +0200

    Añadido activity Ayuda

--- app/src/main/java/view/ViewAyuda.java
+++ app/src/main/java/view/ViewAyuda.java
@@ -2,14 +2,21 @@ package view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.project.francisco.drivealert.R;
 
 public class ViewAyuda extends AppCompatActivity {
 
+    public TextView tvAyuda;
+    public TextView tvAyudaDescripcion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        tvAyuda = (TextView) findViewById(R.id.tvHelp);
+        tvAyudaDescripcion = (TextView) findViewById(R.id.tvHelpDescription);
     }
 }
