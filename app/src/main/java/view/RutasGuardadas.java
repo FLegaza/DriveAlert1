f256a8d19817e1a7127a9bad374558b9e71e927c
FLegaza <flegazab@gmail.com>
2017-08-19 19:18:27 +0200

    Rutas Guardadas

--- app/src/main/java/view/RutasGuardadas.java
+++ app/src/main/java/view/RutasGuardadas.java
@@ -2,6 +2,8 @@ package view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.project.francisco.drivealert.R;
 
@@ -11,10 +13,16 @@ CLASE SAVEROUTES - Muestra las rutas guardadas en la BD
  */
 public class RutasGuardadas extends AppCompatActivity {
 
+    public ListView lvRutas;
+    public TextView tvRutas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_routes);
+
+        lvRutas = (ListView) findViewById(R.id.lvRutas);
+        tvRutas = (TextView) findViewById(R.id.tvRutas);
 
         // LIST VIEW LA MEJOR OPCIÓN para Mostrar una lista de:
         // RUTA 1 - Origen-Destino - Km - Tiempo - Incidencias: 2)
